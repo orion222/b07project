4f33e544a4049243b2110a117d47c04f2cd3c342
niiicky <85355923+niiicky@users.noreply.github.com>
2024-07-20 10:32:19 -0400

    linked firebase to login popup

--- app/src/main/java/com/example/b07demosummer2024/LoginPopup.java
+++ app/src/main/java/com/example/b07demosummer2024/LoginPopup.java
@@ -3,6 +3,7 @@ package com.example.b07demosummer2024;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -15,11 +16,17 @@ import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class LoginPopup extends DialogFragment {
 
-    // temp, idk if theres supposed to be a list of valid logins etc
-    private final String username = "admin";
-    private final String password = "password";
+    private FirebaseDatabase db;
+    private String username;
+    private String password;
     private static boolean admin;
 
     @NonNull
@@ -55,14 +62,36 @@ public class LoginPopup extends DialogFragment {
     }
 
     private boolean checkLogin(EditText a, EditText b) {
+        db = FirebaseDatabase.getInstance("https://b07proj-default-rtdb.firebaseio.com/");
+        DatabaseReference myRef = db.getReference("admin");
+
         String username = a.getText().toString();
         String password = b.getText().toString();
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    setCredentials(snapshot.getKey(), snapshot.getValue(String.class));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("firebase", "Failed to read value (admin).", error.toException());
+            }
+        });
 
         return username.equals(this.username) && password.equals(this.password);
     }
 
     protected boolean isAdmin() {
         return admin;
+    }
+
+    protected void setCredentials(String a, String b) {
+        this.username = a;
+        this.password = b;
     }
 
     protected void setAdmin(boolean c) {
