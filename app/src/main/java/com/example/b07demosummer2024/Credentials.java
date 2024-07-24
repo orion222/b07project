4383c759444d06ded530402d7ca790328e822dd9
niiicky <85355923+niiicky@users.noreply.github.com>
2024-07-24 16:33:04 -0400

    feat: login can handle different credentials from firebase

---
+++ app/src/main/java/com/example/b07demosummer2024/Credentials.java
@@ -0,0 +1,45 @@
+package com.example.b07demosummer2024;
+
+public class Credentials {
+
+    private int id;
+    private String username;
+    private String password;
+
+    public Credentials() {}
+
+    public Credentials(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString(){
+        return getId() + " " + getUsername() + " " + getPassword();
+    }
+}
