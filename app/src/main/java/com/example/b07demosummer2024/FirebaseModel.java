879328a05ebcecd7dac19c65778a53efc6b8bc07
Zohair Syed <1syedzohair@gmail.com>
2024-07-25 18:15:30 -0400

    refactored some stuff

---
+++ app/src/main/java/com/example/b07demosummer2024/FirebaseModel.java
@@ -0,0 +1,9 @@
+package com.example.b07demosummer2024;
+
+public class FirebaseModel implements LoginMVP.Model{
+
+    @Override
+    public void login(String username, String password, LoginCallback callback) {
+
+    }
+}
