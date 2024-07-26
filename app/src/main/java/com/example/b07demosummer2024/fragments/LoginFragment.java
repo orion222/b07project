528d918fc0b1bdd9ef730b7dceb5e64dc0ce0d4f
Orion Chen <orion.chen@hotmail.com>
2024-07-26 17:28:20 -0400

    feat: made homeactivity, recyclerview and other fragments will now be embedded to this activity

--- app/src/main/java/com/example/b07demosummer2024/fragments/LoginFragment.java
+++ app/src/main/java/com/example/b07demosummer2024/fragments/LoginFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+import android.content.Intent;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.b07demosummer2024.R;
+import com.example.b07demosummer2024.activities.HomeActivity;
 
 public class LoginFragment extends Fragment {
 
@@ -41,7 +43,7 @@ public class LoginFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getContext(), "Guest login clicked", Toast.LENGTH_SHORT).show();
-                loadFragment(new RecyclerViewFragment());
+                startActivity(new Intent(getContext(), HomeActivity.class));
             }
         });
 
