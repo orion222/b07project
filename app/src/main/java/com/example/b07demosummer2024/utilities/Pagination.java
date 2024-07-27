5f016a83de18be0fb1aae49dc79ee13785ec5feb
niiicky <85355923+niiicky@users.noreply.github.com>
2024-07-27 13:38:42 -0400

    feat: added pagination to item viewing

---
+++ app/src/main/java/com/example/b07demosummer2024/utilities/Pagination.java
@@ -0,0 +1,38 @@
+package com.example.b07demosummer2024.utilities;
+
+import com.example.b07demosummer2024.models.Item;
+import java.util.*;
+
+public class Pagination {
+    // can switch num of items to display per page
+    private static final int ITEMS_PER_PAGE = 21;
+
+    public static List<Item> generatePage(int currentPage, List<Item> itemList) {
+        if(itemList == null) return new ArrayList<Item>();
+
+        int numItems = itemList.size();
+
+        int startingItem = currentPage * ITEMS_PER_PAGE;
+        int lastPage = numItems / ITEMS_PER_PAGE;
+        int itemsRemaining = numItems % ITEMS_PER_PAGE;
+
+        List<Item> itemsToDisplay = new ArrayList<Item>();
+
+        if(currentPage < lastPage) {
+            for(int i = startingItem; i < startingItem + ITEMS_PER_PAGE; i++) {
+                itemsToDisplay.add(itemList.get(i));
+            }
+        }
+        else if(itemsRemaining > 0) {
+            for(int i = startingItem; i < startingItem + itemsRemaining; i++) {
+                itemsToDisplay.add(itemList.get(i));
+            }
+        }
+
+        return itemsToDisplay;
+    }
+
+    public static int getItemsPerPage() {
+        return ITEMS_PER_PAGE;
+    }
+}
